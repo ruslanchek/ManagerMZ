9f1a5e3f8e929b1320f9891f2d32e079ae4a11b1
ruslanchek <rshashkov@icloud.com>
2015-03-31 20:11:53 +0300

    Init

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "client/css"
+sass_dir = "client/sass"
+images_dir = "client/images"
+javascripts_dir = "client/scripts"
+fonts_dir = "client/fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :sass
